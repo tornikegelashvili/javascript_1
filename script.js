36c59d035a16821d96633fc842c4f070388f8444
tornikegelashvili <119808553+tornikegelashvili@users.noreply.github.com>
2023-02-03 00:49:35 +0400

    create script js

---
+++ script.js
@@ -0,0 +1,22 @@
+let user = "giorgi";
+let surName="modebadze";
+let age=35;
+console.log(user + " " + surName);
+ console.log(`${user} ${surName}`);
+
+ let x=100;
+ let y=20;
+ let sum=x+y;
+ let div=x*y;
+  console.log(sum);
+  console.log(div);
+
+let a=7;
+let b=5;
+let sym=a+b;
+console.log(sym);
+
+let c=7;
+let d=x
+const sam=c*d
+console.log(sam)
